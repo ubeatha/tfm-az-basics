43f05632ace40980e0623f190859db4d17a5bd56
Andrew <20050899+ubeatha@users.noreply.github.com>
2021-10-11 16:27:03 -0400

    Adding output for resource names

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,29 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.my.name
+}
+
 output "resource_group_id" {
   value = azurerm_resource_group.my.id
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.my.name
 }
 
 output "storage_account_id" {
   value = azurerm_storage_account.my.id
 }
 
+output "log_analytics_workspace_name" {
+  value = azurerm_log_analytics_workspace.my.name
+}
+
 output "log_analytics_workspace_id" {
   value = azurerm_log_analytics_workspace.my.id
+}
+
+output "log_analytics_solution_name" {
+  value = azurerm_log_analytics_solution.my.name
 }
 
 output "log_analytics_solution_id" {
@@ -15,13 +31,13 @@ output "log_analytics_solution_id" {
 }
 
 output "group_admin_id" {
-  value = azuread_group.admin.id
+  value = azuread_group.admin.object_id 
 }
 
 output "group_dev_id" {
-  value = azuread_group.dev.id
+  value = azuread_group.dev.object_id 
 }
 
 output "group_test_id" {
-  value = azuread_group.test.id
+  value = azuread_group.test.object_id 
 }
